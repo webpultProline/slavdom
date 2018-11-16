bd60a1f8ba3a7b923c0aa4ad0162081de5dbf368
Webpult <m.ifutin@webpult.pro>
2018-11-16 18:56:50 +0500

    добавил лк

--- js/new.js
+++ js/new.js
@@ -65,15 +65,87 @@ $(function(){
 	///
 	///
 	function setStatusForInput(self){
-		if(self.val().length < 1 || self.val() == ''){
-			self.parent().find('.new--indicator_').removeClass('checked');
-			self.parent().addClass('error');
+		var checkValPassword = self.parents('.new--form_user').find('.new_label--inputs-password').not(self.parents('.new_label--inputs-password'));
+		var valuePassword = checkValPassword.find('input[type="password"]').val();
+		if(valuePassword != '' || self.val() != ''){
+			if(self.val() == valuePassword && self.val() != '' && valuePassword != ''){
+				self.parents('.new--form_user').find('.new_label--inputs-password').find('.new--indicator_').addClass('checked');
+				self.parents('.new--form_user').find('.new_label--inputs-password').find('.new--indicator_input').removeClass('error');
+			} else {
+				self.parents('.new--form_user').find('.new_label--inputs-password').find('.new--indicator_').removeClass('checked');
+				self.parents('.new--form_user').find('.new_label--inputs-password').find('.new--indicator_input').addClass('error');
+			}
 		} else {
-			self.parent().find('.new--indicator_').addClass('checked');
-			self.parent().removeClass('error');
+			self.parents('.new--form_user').find('.new_label--inputs-password').find('.new--indicator_input').removeClass('error');
 		}
 	}
-	$('.new--indicator_input input').change(function(){
+	$('.new_label--inputs-password input[type="password"]').change(function(){
 		setStatusForInput($(this))
 	});
+	///----------------------
+	///Табы в личном кабинете
+	///----------------------
+	///window.location.hash.replace("#","")
+	checkOpenTab('users--tabs',window.location.hash.replace("#",""));
+	
+	$('.new--tabs_btn , .new--tabs_btn-mobile').click(function(){
+		if(typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href') != ''){
+			$('.breadcrumbs.new_show_tablet-block .breadcrumbs__current-page').text('');
+			$('.breadcrumbs.new_show_tablet-block .breadcrumbs__current-page').text($(this).text());
+			
+			$($(this).attr('href')).parents('.new--tabs').find('.new--tab').removeClass('open--tab');
+			$($(this).attr('href')).addClass('open--tab');
+			
+
+			var offsetTopPlus = $('.header-floating').outerHeight(true);
+			
+			$([document.documentElement, document.body]).stop().animate({
+				scrollTop: $($(this).attr('href')).offset().top-offsetTopPlus
+			}, 200);
+			
+			$(this).parent().find('.new--tabs_btn').removeClass('active');
+			$(this).addClass('active');
+		}
+	});
+	
+	function checkOpenTab(parent_tab,tab){
+		if(tab != ''){
+			$('.new--tabs_btn').each(function(){
+				if(typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href') != ''){
+					var links = $(this).attr('href').replace('#', '');
+					if(links != tab){
+						$(this).removeClass('active');
+					} else {
+						$(this).addClass('active');
+						$('.breadcrumbs.new_show_tablet-block .breadcrumbs__current-page').text('');
+						$('.breadcrumbs.new_show_tablet-block .breadcrumbs__current-page').text($(this).text());
+					}
+				}
+			});
+			
+			$("#"+parent_tab).find('.new--tab').removeClass('open--tab');
+			$("#"+parent_tab).find("#"+tab).addClass('open--tab');
+			
+			var offsetTopPlus = $('.header-floating').outerHeight(true);
+			
+			$([document.documentElement, document.body]).stop().animate({
+				scrollTop: $("#"+parent_tab).find("#"+tab).offset().top-offsetTopPlus
+			}, 200);
+		} else {
+			var firstTab = $("#"+parent_tab).find('.new--tab').first();
+			var links = firstTab.attr('id');
+			firstTab.addClass('open--tab');
+			$('.new--tabs_btn').each(function(){
+				if(typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href') != ''){
+					var link_btn = $(this).attr('href').replace('#', '');
+					console.log(links);
+					if(links != link_btn){
+						$(this).removeClass('active');
+					} else {
+						$(this).addClass('active');
+					}
+				}
+			});
+		}
+	}
 });
